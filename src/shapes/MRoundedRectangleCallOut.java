8187cdb17b7696af22086f0b7c55b0164cefc2be
Hakim <Hakim@DESKTOP-S3DMKHA>
2020-01-09 12:28:25 +0100

    Ajout d'une nouvelle forme

--- src/shapes/MRoundedRectangleCallOut.java
+++ src/shapes/MRoundedRectangleCallOut.java
@@ -20,37 +20,47 @@ public class MRoundedRectangleCallOut extends MPointsShape {
 
     @Override
     public void shapeFill(Graphics2D g) {
-        Rectangle rect = getBounds();
-        int height = rect.height - (rect.height / 6);
-        g.fillRoundRect(rect.x, rect.y, rect.width, height, rect.width / 4, height / 4);
-        int xPoints[] = {rect.x + (rect.width / 5), rect.x + (rect.width / 4), rect.x + (rect.width / 3)};
-        int yPoints[] = new int[]{rect.y + height - 2, rect.y + rect.height, rect.y + height - 2};
-        g.fillPolygon(xPoints, yPoints, 3);
+        int xPoints[] = {
+        		start.y + (end.y - start.y) * 1 / 4,
+                end.x *3/ 4,
+                end.x,
+                end.x,
+                start.x + (end.x - start.x) / 2,
+                start.y + (end.y - start.y) * 1 / 4,
+                start.x,
+                start.x};
+        int yPoints[] = {
+        		start.y,
+                start.y,
+                start.y + (end.y - start.y) * 1 / 4,
+                start.y + (end.y - start.y) * 3 / 4,
+                end.y,
+                end.y,
+                start.y + (end.y - start.y) * 3 / 4,
+                start.y + (end.y - start.y) * 1 / 4};
+        g.fillPolygon(xPoints, yPoints, 7);
     }
 
     @Override
     public void shapeOutline(Graphics2D g) {
-        Rectangle rect = getBounds();
-        int height = rect.height - (rect.height / 6);
-        g.drawRoundRect(rect.x, rect.y, rect.width, height, rect.width / 4, height / 4);
-        int xPoints[] = {rect.x + (rect.width / 5), rect.x + (rect.width / 4), rect.x + (rect.width / 3)};
-        int yPoints[] = new int[]{rect.y + height - 2, rect.y + rect.height, rect.y + height - 2};
-        g.drawPolyline(xPoints, yPoints, 3);
-        Polygon p = new Polygon(xPoints, yPoints, 3);
-        for (int i = 0; i < rect.width; i++) {
-            for (int j = 0; j < rect.height; j++) {
-                Point point = new Point(rect.x + i, rect.y + j);
-                if (p.contains(point)) {
-                    Color color;
-                    if (fill) {
-                        color = color2;
-                    } else {
-                        color = new Color(0, 0, 0, 0);
-                    }
-                    canvas.getGlass().setRGB(point.x, point.y, color.getRGB());
-                }
-            }
-        }
-        g.drawPolyline(xPoints, yPoints, 3);
+        int xPoints[] = {
+        	end.x * 1 / 4,
+        	end.x *3/ 4,
+            end.x,
+            end.x,
+            end.x*3/4,
+            end.x * 1 / 4,
+            start.x,
+            start.x};
+        int yPoints[] = {
+        	start.y,
+            start.y,
+            start.y + (end.y - start.y) * 1 / 4,
+            start.y + (end.y - start.y) * 3 / 4,
+            end.y,
+            end.y,
+            start.y + (end.y - start.y) * 3 / 4,
+            start.y + (end.y - start.y) * 1 / 4};
+        g.drawPolygon(xPoints, yPoints, 7);
     }
 }
